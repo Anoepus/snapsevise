7955dd86b357de1f178773f3cb0b35c732713a98
Anoepus <ansu0001@stud.kea.dk>
2026-01-04 15:56:19 +0100

    anden commit

---
+++ src/main/java/org/example/snapsevise/SnapseviseApplication.java
@@ -0,0 +1,13 @@
+package org.example.snapsevise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnapseviseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnapseviseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/snapsevise/SnapseviseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.snapsevise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnapseviseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
